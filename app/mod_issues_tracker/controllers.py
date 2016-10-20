ef3605697a1d138dcbe5ec418d1dd14a14322050
white_viel <rotychbrian@gmail.com>
2016-10-21 02:57:11 +0300

    Recreated the issues list table

--- app/mod_issues_tracker/controllers.py
+++ app/mod_issues_tracker/controllers.py
@@ -7,7 +7,6 @@ from sqlalchemy import text
 from app import db
 from app.mod_issues_tracker.views import NewIssueForm, AssignIssueForm
 from app.models import Issue, User, Comment, AssignedIssue, IssueStatus
-from sqlalchemy import update
 
 mod_issue_tracker = Blueprint('issues', __name__, url_prefix='/issues')
 
@@ -49,11 +48,15 @@ def add_new_issue():
 def get_issues():
 
     if current_user.is_admin():
-        issues_list = db.session.query(Issue, User).filter(User.id == Issue.user).all()
+        issues_list = db.session.query(Issue.id, Issue.title, Issue.priority, \
+                                       Issue.user, Issue.status, User.fullname) \
+            .outerjoin(User, Issue.user == User.id)
     else:
         user_id = current_user.get_id()
-        issues_list = db.session.query(Issue, User).filter(User.id == Issue.user, Issue.user == user_id)\
-            .all()
+        issues_list = db.session.query(Issue.id, Issue.title, Issue.description, Issue.priority, \
+                                       Issue.user, Issue.status, User.fullname, User.id) \
+            .outerjoin(User, Issue.user == User.id) \
+            .filter(User.id == Issue.user, Issue.user == user_id).all()
 
     return render_template('issues/issues_list.html', issues_list=issues_list)
 
@@ -69,14 +72,14 @@ def get_issue_by_id(issue_id):
         action_issue = form.action_issue.data
 
         if int(action_issue) == IssueStatus.CLOSED:
-            db.engine.execute(text("UPDATE Issues SET status = "+ str(action_issue) +" WHERE id = " + str(issue_id)))
-            flash("Closed Issue #"+str(issue_id))
+            db.engine.execute(text("UPDATE Issues SET status = " + str(action_issue) + " WHERE id = " + str(issue_id)))
+            flash("Closed Issue #" + str(issue_id))
         elif int(action_issue) == IssueStatus.OPEN:
             db.engine.execute(text("UPDATE Issues SET status = " + str(action_issue) + " WHERE id = " + str(issue_id)))
             flash("Reopened Issue #" + str(issue_id))
         elif int(action_issue) == IssueStatus.IN_PROGRESS:
             db.engine.execute(text("UPDATE Issues SET status = " + str(action_issue) + " WHERE id = " + str(issue_id)))
-            flash("Closed Issue #" + str(issue_id) )
+            flash("Closed Issue #" + str(issue_id))
 
         print action_issue
 
