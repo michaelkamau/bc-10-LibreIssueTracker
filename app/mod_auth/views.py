9d4f4cd0a1ecefe301287a17620e57019f06d8ff
white_viel <rotychbrian@gmail.com>
2016-10-20 16:33:34 +0300

    Added logic to manage the login/logout button depending on user log state

--- app/mod_auth/views.py
+++ app/mod_auth/views.py
@@ -1,13 +1,13 @@
 from wtforms import Form, PasswordField, StringField, SelectField
+from wtforms.validators import DataRequired, Email
 
 from app.models import Department
 
 
 class RegistrationForm(Form):
     username = StringField(id='username')
-    email = StringField(id='email')
+    email = StringField(id='email', validators=[DataRequired(), Email()])
     password = PasswordField(id='password')
-    confirm_password = PasswordField(id='confirm_password')
     first_name = StringField(id='first_name')
     last_name = StringField(id='last_name')
     department = SelectField(id='department',
--- app/mod_index/controller.py
+++ app/mod_index/controller.py
@@ -1,10 +1,10 @@
 from flask import Response
 from flask import render_template, app, request, flash, redirect, url_for, Blueprint
-
+from flask_login import current_user
 
 mod_index = Blueprint('index', __name__)
 
 
 @mod_index.route('/', methods=['GET'])
 def index():
-    return Response(response=render_template('static/base.html'), status=200)
+    return Response(response=render_template('static/base.html'))
